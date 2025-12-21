50eaeb06114de21dc0f2b8071cc94dc434466ffd
Santhosh Sivakumar <2005.santhoshsivakumar@gmail.com>
2025-12-21 11:57:46 +0530

    New Feature #19: Created App Bar Skeleton
    Changes Made:
    - Created App Bar Skeleton
    - Made App Bar responsive
    
    Works in:
    - Mobile
    - Tablet
    - Desktop
    - Large Desktop

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 // Packages
+import 'package:flutter_breakpoints/flutter_breakpoints.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hugeicons_pro/hugeicons.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,8 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final isMobile = Breakpoints.mobile.isBreakpoint(context);
+
     ref.listen(authStateProvider, (_, user) {
       if (user.value == null) {
         Navigator.pushNamedAndRemoveUntil(context, '/welcome', (_) => false);
@@ -19,15 +22,47 @@ class HomePage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: kToolbarHeight + 16,
+        backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
+        centerTitle: false,
+        title: isMobile
+            ? Text("Organizations")
+            : Row(
+                children: [
+                  Text("Organizations"),
+                  const SizedBox(width: 18),
+                  Expanded(child: HomeSearchBar()),
+                ],
+              ),
         actions: [
           IconButton(
-            icon: Icon(HugeIconsStroke.logout01),
             onPressed: () {
               ref.read(authControllerProvider.notifier).logout();
             },
+            icon: Icon(HugeIconsStroke.logout01),
           ),
         ],
+        bottom: isMobile
+            ? PreferredSize(
+                preferredSize: Size.fromHeight(56),
+                child: Padding(padding: EdgeInsetsGeometry.all(8), child: HomeSearchBar()),
+              )
+            : null,
       ),
+    );
+  }
+}
+
+class HomeSearchBar extends StatelessWidget {
+  const HomeSearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SearchBar(
+      elevation: WidgetStatePropertyAll(0),
+      trailing: [
+        Padding(padding: const EdgeInsets.all(8.0), child: Icon(HugeIconsStroke.search01)),
+      ],
     );
   }
 }
